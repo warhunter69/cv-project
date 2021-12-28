a8ff8be86d2873ee2ede0c1c898f27626d558ea1
ahmed <warhunter69@gmail.com>
2021-12-28 12:31:47 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {firstName: '',lastName:'',email: ''};
+    
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    //this.setState({firstName: event.target.value});
+    this.setState({
+      // Computed property names
+      // keys of the objects are computed dynamically
+      [event.target.name] : event.target.value
+   })
+   console.log(this.state)
+    
+  }
+  handleSubmit(event){
+    this.setState({[event.target.name] : event.target.value});
+    event.preventDefault();
+  }
 
-function App() {
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <label className="panel">panel</label>
+      </div>
+      <form onSubmit={this.handleSubmit}>
+        <div>
+        <label>first name:
+        <input type="text" name="firstName" value = {this.state.firstName} onChange={this.handleChange}/>
+        </label>
+        <label>last name: 
+        <input type="text" name="lastName" value = {this.state.lastName} onChange={this.handleChange}/>
+        </label>
+        <label>email:
+          <input type="email" name="email" value = {this.state.email} onChange={this.handleChange}/>
+        </label>
+        <input type="submit" value="Submit" />
+        </div>
+      </form>
     </div>
   );
-}
+}}
 
 export default App;
